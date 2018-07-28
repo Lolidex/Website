f4145f5254a2490921177bcac03ce3b406ddb59d
Léandre <leandre.blanchard@epitech.eu>
2018-07-28 23:48:55 +0200

    [HTML / Javascript] button now triggers a javascript function, that gets the loliname

---
+++ javascript/search.js
@@ -0,0 +1,4 @@
+$('#search_loli_button').on('click', function (e) {
+    var loliname = document.getElementById("search_loliname").value;
+    alert("Searching loli named: \"" + loliname + "\"");
+})
