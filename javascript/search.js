80dfec8501f8794e99b71c219248b81e5761b179
Léandre <leandre.blanchard@epitech.eu>
2018-07-31 14:50:08 +0200

    [HTML + JS] change button type to "submit" and return false in js-on-click

--- javascript/search.js
+++ javascript/search.js
@@ -4,4 +4,5 @@ var search_bar = document.getElementById("search_loliname");
 $('#search_loli_button').on('click', function () {
     var loliname = search_bar.value;
     location.replace("lolisearch.html");
+    return (false);
 })
