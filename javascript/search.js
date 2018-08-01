81d714629dadcdadb58a2ff49c8471352a7da0a1
Indra <wuppie55@gmail.com>
2018-08-01 02:07:15 +0200

    Changes to index.html - added prevent loli background image from being dragged - remove unnecessary javascript from index - added margin between search input and button - reformatted document - renamed "ressources" to "resources"

--- javascript/search.js
+++
@@ -1,8 +0,0 @@
-
-var search_bar = document.getElementById("search_loliname");
-
-$('#search_loli_button').on('click', function () {
-    var loliname = search_bar.value;
-    location.replace("lolisearch.html");
-    return (false);
-})
